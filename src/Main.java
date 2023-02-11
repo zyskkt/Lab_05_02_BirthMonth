2b8e34e4b528cad122bd333e749a1a1ef7b471c2
Kent Zysk <kent@kentzysk.com>
2023-02-11 01:11:38 -0600

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Init variable
+        int birthMonth = 3;
+        //Output asking for birth month
+        System.out.println("What is you birth month? (1-12)");
+            //If the birth month is over 12, let user know it's out of range.
+            if (birthMonth > 12) {
+                System.out.println("You incorrect month value: " + birthMonth);
+                //If the birth month is under 1, let user know it's out of range.
+            } else if (birthMonth < 1) {
+                System.out.println("You entered an incorrect month value: " + birthMonth);
+            } else {
+                //Birth month in range, print result.
+                System.out.println("Your birth month is " + birthMonth);
+            }
     }
 }
+
